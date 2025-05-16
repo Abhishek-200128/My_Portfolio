6f388dfc819af1a3844afff8a450e9f9c3934f24
asiv0019 <abhisheksiva28@gmail.com>
2025-05-16 18:48:48 +1000

    Remove Resume

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -31,7 +31,6 @@ export default function Greeting(props) {
               <SocialMedia theme={theme} />
               {/* <div className="button-greeting-div">
               <Button text="Contact me" href="#contact" />
-              <Button text="See my resume" newTab={true} href={greeting.resumeLink} />
             </div> */}
             </div>
           </div>
